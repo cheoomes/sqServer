24777ad0c2e09d78fd22491e8c220e8b88558167
cheoomes <99665809+cheoomes@users.noreply.github.com>
2026-03-25 22:41:12 +0100

    prisma.ts setup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// @ts-ignore
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
